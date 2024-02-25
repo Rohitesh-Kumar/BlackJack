4e41be9489cf47ade40536d8882d722baa0e9c2c
Rohitesh Kumar <148290956+Rohitesh-Kumar@users.noreply.github.com>
2024-02-25 15:02:03 +1200

    Add files via upload

---
+++ BlackJack/index.js
@@ -0,0 +1,14 @@
+let slideIndex = 0; // Start with the first image
+
+  function showSlides() {
+    let slides = document.getElementsByClassName("slider-image");
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2500); // Change image every 2 seconds
+  }
+
+  showSlides();
